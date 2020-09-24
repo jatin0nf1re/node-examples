ee2cce20c8983678050f8e80899ef8cd3279d45f
jatin0nf1re <its.me.jatinsingh@gmail.com>
2020-09-24 11:24:06 +0530

    using require

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-var rect = {
-    perimeter : (x,y) => (2*(x+y)),
-    area : (x,y) => (x*y)
-};
+var rect = require('./node-examples/rect.js');
 
 function solveRect(l,b){
     if(l<=0 || b<=0){
         console.log("Invalid values of dimension");
     }else{
         console.log("Area of rectangle is - " + rect.area(l,b));
-        console.log("Perimeter of recanle is - " + rect.perimeter);
+        console.log("Perimeter of rectangle is - " + rect.perimeter(l,b));
     }
 }
 
